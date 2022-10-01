425ce227383439a99aad12fe8f5fdf7e1a8d1bb6
Karona Srun <karonasrun.ks@gmail.com>
2022-10-01 10:24:55 +0700

    rename file logo.png to Logo.png in welcome.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -37,7 +37,7 @@
                 </button>
                 <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
                     <a href="{{ url('/') }}">
-                        <img src="{{ asset('logo/logo.png') }}" width="110" height="32" alt="Karonasrun"
+                        <img src="{{ asset('logo/Logo.png') }}" width="110" height="32" alt="Karonasrun"
                             class="navbar-brand-image"> Karonasrun
                     </a>
                 </h1>
